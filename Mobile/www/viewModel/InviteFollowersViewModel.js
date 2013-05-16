5b5cdccdeeb9030e57ceda1b883c3862c52acb76
Jared DeGraffenried <degraffenried@gmail.com>
2013-05-16 13:59:46 -0600

    cleaning up a bunch of loading notifications

---
+++ Mobile/www/viewModel/InviteFollowersViewModel.js
@@ -0,0 +1,69 @@
+﻿/*globals ko*/
+
+function InviteFollowersViewModel() {
+    
+    // --- properties
+    
+    var that = this;
+    var  dataService = new EvernymChannelService();
+	
+    this.template = "inviteFollowersView";
+    
+    
+	this.channelid = ko.observable();
+    this.channelname = ko.observable();
+    
+    
+    
+    $("#" + this.template).live("pagebeforeshow", function(e, data){
+                                
+                                
+                                
+                                
+                                if ($.mobile.pageData && $.mobile.pageData.id){
+                                    that.activate({id:$.mobile.pageData.id});
+                                }
+                                
+                                else {
+                                
+                                    var currentChannel = localStorage.getItem("currentChannel");
+                                    var lchannel = JSON.parse(currentChannel);
+                                    that.activate(lchannel);
+                                
+                                }
+                                
+                                
+                                });
+    
+    
+    // Methods
+    
+    function getChannelFromPageData(){
+        that.activate({id:$.mobile.pageData.id});
+    }
+    
+    this.logoutCommand = function(){
+        loginViewModel.logoutCommand();
+        $.mobile.changePage("#" + loginViewModel.template)
+        
+    }
+	
+    this.activate = function (channel) {
+        
+        that.channelid(channel.id);
+        that.channelname(channel.name);
+        
+        
+        return true;
+	    
+    };
+
+	
+
+
+
+   
+
+    
+    
+}
