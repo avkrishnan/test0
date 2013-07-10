8503c29f2836e7fbb0bbaecc6f4132fac7f0fa8a
Jared DeGraffenried <degraffenried@gmail.com>
2013-07-09 19:03:48 -0600

    adding stuff for email verification

--- Mobile/www/Test/web api/webapi-account-cud.test.js
+++ Mobile/www/Test/web api/webapi-account-cud.test.js
@@ -4,7 +4,7 @@
 	var okAsync = QUnit.okAsync,
 		stringformat = QUnit.stringformat;
 
-	var baseUrl = 'http://qupler.no-ip.org:8080/api12/rest', // Test environment
+	var baseUrl = 'http://qupler.no-ip.org:8080/api15/rest', // Test environment
 	//var baseUrl = 'http://localhost:8080/api/rest', //production environment
 	//var baseUrl = 'http://192.168.1.202:8080/catalyst-api/rest', //local route to production environment
 	getMsgPrefix = function (id, rqstUrl) {
@@ -103,17 +103,30 @@
 	  
 	  var account = generateAccount();
 	  var accessToken;
+          
+      var times = 0;
+         
 	  
 	  enroll(account, expectSuccessNoContent, step2);
 	  
 	  function step2() {
+          
 		  login(generateLogin(account), expectSuccess, step3);
 	  }
-	  
-	  function step3(data) {
-		  
-		  forgot({accountName: account.accountName, emailAddress: account.emailaddress}, expectSuccessNoContent);
-	  }
+                    
+      function step3(){
+         
+          checkEmailAndVerify(account.emailaddress, step4);
+      }
+
+          
+        function step4(data){
+        
+                  
+          forgot({accountName: account.accountName, emailAddress: account.emailaddress}, expectSuccessNoContent, function(){console.log('done')});
+              start();
+          }
+     
 	  
 	  });
 
@@ -121,6 +134,7 @@
 		stop(timoutms); //tell qunit to wait 5 seconds before timing out
 
 		var account = generateAccount();
+         
 
 		account.accountName =
 			'01234567890123456789012345678901234567890123456789' +
@@ -141,39 +155,112 @@
 		stop(timoutms); //tell qunit to wait 5 seconds before timing out
 
 		var account = generateAccount();
-
 		account.accountName = 'hi';
-
 		enroll(account, expectBadRequest);
 	});
+ 
+ 
+    function findKeyForVerification(html){
+        var regex = /<a *href=".*\?key=(.*)"/ig;
+        var results = regex.exec(html);
+        
+        return results[1];
+    }
 
 
-
-	function enroll(account, handler, postHandlerCallback) {
-		callAPI('POST', '/account/enroll', null, account ? account : generateAccount(), handler, postHandlerCallback);
+    function verifyEmail(verification_key, handler, postHandlerCallback){
+        callAPI('PUT', '/commethod/verification/' + verification_key, undefined, handler, postHandlerCallback);
+    }
+ 
+    function enroll(account, handler, postHandlerCallback) {
+		callAPI('POST', '/account/enroll', undefined, account ? account : generateAccount(), handler, postHandlerCallback);
 	}
  
 	function forgot(account, handler, postHandlerCallback) {
-		callAPI('POST', '/account/forgot', null, account, handler, postHandlerCallback);
+		callAPI('POST', '/account/forgot', undefined, account, handler, postHandlerCallback);
 	}
 
 	function login(loginRequest, handler, postHandlerCallback) {
-		callAPI('POST', '/account/login', null, loginRequest, handler, postHandlerCallback);
+		callAPI('POST', '/account/login', undefined, loginRequest, handler, postHandlerCallback);
 	}
 
 	function logout(accessToken, handler, postHandlerCallback) {
-		callAPI('POST', '/account/logout', accessToken, null, handler, postHandlerCallback);
+		callAPI('POST', '/account/logout', accessToken, undefined, handler, postHandlerCallback);
 	}
+ 
+    function checkEmail(emailAddress, handler, postHandlerCallback){
 
+        var times = 0;
+        function waitForIt(){
+ 
+            var promise = timeoutPromise(1000);
+            promise.done(function() {
+              getEmail();
+            });
+ 
+         }
+ 
+         function timeoutPromise(millis) {
+             var deferred = $.Deferred();
+             setTimeout(function() {
+                deferred.resolve();
+                }, millis);
+             return deferred.promise();
+         }
+ 
+ 
+         function checkForContent(data){
+             if (data.length == 0){
+ 
+                 waitForIt();
+                 times ++;
+                 console.log('no email. times tried: ' + times);
+                 return;
+             }
+ 
+             postHandlerCallback(data);
+ 
+         }
+ 
+         function getEmail(){
+             callAPI('GET', '/test/msg/' + emailAddress, null, null, handler, checkForContent, "html");
+         }
+ 
+         getEmail();
+ 
+    }
+ 
+ 
+    function checkEmailAndVerify(emailAddress, postHandlerCallback){
+ 
+        console.log('checking email');
+        checkEmail(emailAddress,expectSuccess, verify);
+ 
+        function verify(data) {
+ 
+            console.log('email: ' + data);
+ 
+            var key = findKeyForVerification(data);
+            console.log('verification key: ' + key);
+ 
+            verifyEmail(key, expectSuccessNoContent, postHandlerCallback);
+        }
+ 
+    }
 
 
 	// generic helper method to handle ajax calls to API
-	function callAPI(method, resource, accessToken, object, handler, postHandlerCallback) {
-		var ajaxParams = {
+	function callAPI(method, resource, accessToken, object, handler, postHandlerCallback, datatype) {
+	
+        if (!datatype){
+            datatype = "json";
+        }
+ 
+       var ajaxParams = {
 			url: baseUrl + resource,
 			type: method,
 			data: JSON.stringify(object),
-			dataType: "json",
+			dataType: datatype,
 			contentType: "application/json"
 		}
 		if (accessToken) {
@@ -185,8 +272,10 @@
 		var ajaxCall = $.ajax(ajaxParams);
 
 		ajaxCall.done(function (data, textStatus, jqXHR) {
-			handler(textStatus, jqXHR.status);
+                      
+                      handler(textStatus, jqXHR.status);
 			callback(data);
+                      
 		});
 
 		ajaxCall.fail(function (jqXHR, textStatus, errorThrown) {
@@ -201,16 +290,22 @@
 				start();
 			}
 		}
+ 
+        return ajaxCall;
 
-	}
+     }
+ 
+ 
 
-	// handlers
-	function expectSuccess(textStatus, status, additionalDetails) {
-		equal(textStatus, 'success', additionalDetails);
-		equal(status, 200);
-	}
-
-	function expectSuccessNoContent(textStatus, status, additionalDetails) {
+ 
+ 
+ // handlers
+ function expectSuccess(textStatus, status, additionalDetails) {
+ equal(textStatus, 'success', additionalDetails);
+ equal(status, 200);
+ }
+ 
+    function expectSuccessNoContent(textStatus, status, additionalDetails) {
 		equal(textStatus, 'nocontent', additionalDetails);
 		equal(status, 204);
 	}
@@ -227,9 +322,12 @@
 
 
 	function generateAccount() {
+ 
+        var strAccountName = 'test-bob' + randomString(8);
+ 
 		return {
-			accountName: 'test-' + randomString(5), // Create Random AccountName Generator
-			emailaddress: 'test-evernym-982346@mailinator.com',
+			accountName: strAccountName, // Create Random AccountName Generator
+			emailaddress: strAccountName + '@rs7292.mailgun.org',
 			password: 'secretshizzle',
 			firstname: 'testFirst',
 			lastname: 'testLast'
@@ -246,7 +344,7 @@
 
 	function randomString(length) {
 		var text = "";
-		var possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		var possibleChars = "abcdefghijklmnopqrstuvwxyz";//"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 		for (var i = 0; i < length; i++)
 			text += possibleChars.charAt(Math.floor(Math.random() * possibleChars.length));
