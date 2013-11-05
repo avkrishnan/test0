9355ba227a0512e35165e9fa7ccf165667e9a834
pradeep kumar <pradeep1991singh@gmail.com>
2013-11-05 17:22:43 +0530

    i own channel list only once (no repeation)

--- Mobile/www/viewModel/ChannelsIOwnViewModel.js
+++ Mobile/www/viewModel/ChannelsIOwnViewModel.js
@@ -54,11 +54,7 @@ function ChannelsIOwnViewModel() {
 	
 	function errorAPI(data, status, details){
 		$.mobile.hidePageLoadingMsg();	
-		if (loginPageIfBadLogin(details.code)){
-		//showMessage('Please log in or register to view channels.');
-		} else {
-			showError('Error listing my channels: ' + details.message);
-		}
+		showError('Error listing my channels: ' + details.message);
 	};
 		
 	this.listMyChannelsCommand = function () {
@@ -84,80 +80,3 @@ function ChannelsIOwnViewModel() {
 		goToView('followersListView');
 	};
 }
-
-/*Todo - remvoed when channesIOwn page is done
-	this.showChannel = function (channel) {
-		localStorage.setItem('currentChannel', JSON.stringify(channel));
-		$.mobile.changePage('#' + channelMenuViewModel.template);
-	};*/
-	/*
-	this.populateChannelList = function(){
-		$.mobile.showPageLoadingMsg('a', 'Loading Channels');
-		return this.listMyChannelsCommand().then(gotChannelList);	
-	};	
-	this.refreshChannelList = function(){
-		$.mobile.showPageLoadingMsg('a', 'Loading Channels');
-		return this.listMyChannelsCommand().then(gotChannels);	
-	};	
-	this.clearForm = function(){
-		that.name('');
-		that.channels.removeAll();
-	};	
-	function gotChannels(data){
-		$.mobile.hidePageLoadingMsg();	
-		if (data.channel && data.channel.constructor == Object){
-			data.channel = [data.channel];
-		}
-		if (!data.channel.length ){
-			$.mobile.changePage('#' + channelNewViewModel.template);
-			$('#no_channels_notification').show();
-			return;
-		}        
-		//populatePanel(data.channel);	
-		//var panelhtml = $('#globalpanel').find('#mypanel').html();
-		//$.mobile.activePage.find('#mypanel').html(panelhtml);
-		//$.mobile.activePage.find('#mypanel').panel();
-		//$.mobile.activePage.find('#mypanel').trigger('create');	
-		//$.mobile.activePage.find('#mypanel').trigger('updatelayout');        
-		$('#no_channels_notification').hide();	
-		that.channels.removeAll();
-		that.channels(data.channel);	
-	};	    
-  function gotChannelList(data){
-		$.mobile.hidePageLoadingMsg();	
-		//that.shown = true;
-		//that.channels.removeAll();	
-		if (data.channel && data.channel.constructor == Object){	
-			data.channel = [data.channel];
-		}	
-		$('#no_channels_notification').hide();	
-		that.channels.removeAll();
-		that.channels(data.channel);	
-	};*/        
-	/*this.showChannelStartMessage = function (channel) {
-		that.showChannel(channel);
-		channelMenuViewModel.initiateNewBroadcast();
-	};    
-	this.newChannelCommand = function () {
-		channelNewViewModel.activate();
-		$.mobile.changePage('#' + channelNewViewModel.template);
-	};*/	
-	/*function successfulCreate(data){
-		$.mobile.hidePageLoadingMsg();
-		$.mobile.changePage('#' + channelListViewModel.template);
-		channelListViewModel.activate();
-	};    
-	function errorCreate(data, status, response){
-		$.mobile.hidePageLoadingMsg();        
-		console.log('error creating channel: ' + response.message);
-		showError('Error creating channel: ' + response.message);
-		loginPageIfBadLogin(details.code);        
-	};  
-	this.logoutCommand = function(){
-		return loginViewModel.logoutCommand();
-		$.mobile.changePage('#' + loginViewModel.template)
-	};   */   
-	/*this.createChannelCommand = function () {	
-		$.mobile.showPageLoadingMsg('a', 'Creating Channel ' + that.name());
-		return ES.channelService.createChannel({name: that.name()}, {success: successfulCreate, error: errorCreate});
-	};*/
