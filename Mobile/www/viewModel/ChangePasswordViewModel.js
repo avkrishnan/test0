4c26e9ba68d50211afcc6ca4be3fb2584c388bb2
pradeep kumar <pradeep1991singh@gmail.com>
2013-11-18 20:32:14 +0530

    working on back navigation on app

--- Mobile/www/viewModel/ChangePasswordViewModel.js
+++ Mobile/www/viewModel/ChangePasswordViewModel.js
@@ -8,20 +8,27 @@ function ChangePasswordViewModel() {
   this.displayname = 'Change Password';
 	
   this.accountName = ko.observable();	
-  this.email = ko.observable();
-  this.errorForgotPassword = ko.observable();
-  this.usernameClass = ko.observable();
-  this.emailClass = ko.observable();
+	this.newPassword = ko.observable();
+	this.newConfirmPassword = ko.observable();
+	this.errorMessageNew = ko.observable();
+	this.errorMessageConfirm = ko.observable();
 	
 	/* Methods */
   this.applyBindings = function () {
     $('#' + that.template).on('pagebeforeshow', null, function (e, data) {     
-			that.activate();		
+			that.newPassword('');
+			that.newConfirmPassword('');
+			that.activate();	
     });
+		$('#changePasswordView input').on("keyup", that.inputKeyUp);
   };
 	
+	this.inputKeyUp = function () {
+		that.errorMessageNew('');
+		that.errorMessageConfirm('');
+	}	
+	
   this.activate = function () {
-		alert(localStorage.getItem("accountName"));
 		var token = ES.evernymService.getAccessToken();
 		if(token == '' || token == null) {
 			that.accountName(localStorage.getItem("accountName"));  
@@ -33,53 +40,25 @@ function ChangePasswordViewModel() {
   };
 	
 	$(document).keyup(function (e) {
-		if (e.keyCode == 13 && $.mobile.activePage.attr('id') == 'forgotPasswordView') {
-			that.forgotPasswordCommand();
+		if (e.keyCode == 13 && $.mobile.activePage.attr('id') == 'changePasswordView') {
+			that.changePassword();
 		}
 	});
 	
-  this.forgotPasswordCommand = function () {
-    var emailReg = /^[\+_a-zA-Z0-9-]+(\.[\+_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$/;
-    if (that.accountName() == '' && that.email() == '') {
-      that.usernameClass('validationerror');
-      that.emailClass('validationerror');
-      that.errorForgotPassword('Please enter username or email');
-    } else if (that.email() != '' && !emailReg.test(that.email())) {
-      that.emailClass('validationerror');
-      that.errorForgotPassword('Please enter valid email');
-    } else {
-      var callbacks = {
-        success: forgotPasswordSuccess,
-        error: forgotPasswordError
-      };
-      var forgotPasswordModel = {};
-      forgotPasswordModel.accountname = that.accountName();
-      forgotPasswordModel.emailAddress = that.email();
-			$.mobile.showPageLoadingMsg('a', 'Sending Forgot Password Request');
-      return ES.loginService.forgotPassword(forgotPasswordModel, callbacks);
-    }
-  };
-
-  function forgotPasswordSuccess(args) {
-    $.mobile.hidePageLoadingMsg();
-		if(that.accountName() == '') {
-			localStorage.setItem('resetAccount', that.email());	
-		} else {
-			localStorage.setItem('resetAccount', that.accountName());				
+  this.changePassword = function () {
+		if(that.newPassword() == '') {
+			that.errorMessageNew("Please input new password!");
 		}
-		goToView('forgotPasswordSuccessView');		
-  }
-
-  function forgotPasswordError(data, status, details) {
-    $.mobile.hidePageLoadingMsg();
-    loginPageIfBadLogin(details.code);
-    if (details) {
-      that.usernameClass('validationerror');
-      that.emailClass('validationerror');
-      that.errorForgotPassword(details.message);
-    } else {
-      showError(details.message);
-    }
-  }
+		else if(that.newConfirmPassword() == '') {
+			that.errorMessageConfirm("Please input new confirm password!");
+		}
+		else if(that.newPassword() != that.newConfirmPassword()) {
+			that.errorMessageConfirm("Password's don't match");
+		}
+		else {
+			alert('matching');
+			return api.callAPI('PUT', '/account/forgot', resetPasswordModel, callbacks);
+		}
+	};
 	
 }
