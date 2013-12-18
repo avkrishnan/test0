9b08cf86294420d160648aa532fb0af86e418af2
pradeep kumar <pradeep1991singh@gmail.com>
2013-12-18 18:13:35 +0530

    back view

--- Mobile/www/viewModel/ChannelMenuViewModel.js
+++ Mobile/www/viewModel/ChannelMenuViewModel.js
@@ -6,12 +6,16 @@ function ChannelMenuViewModel() {
 	this.viewid = 'V-04';
 	this.viewname = 'ChannelMenu';
 	this.displayname = 'Channel Menu';	
-	this.accountName = ko.observable();	
+	this.accountName = ko.observable();
+	
+	/* Channel Menu observable */
+	this.backView = ko.observable();
 	
 	/* Methods */
 	this.applyBindings = function() {
 		$('#' + that.template).on('pagebeforeshow', function (e, data) {
       that.activate();
+			that.backView(data.prevPage.attr('id'));
     });	
 	};  
 	
@@ -27,7 +31,7 @@ function ChannelMenuViewModel() {
 	this.menuHide = function() {
 		backNavText.pop();
 		backNavView.pop();
-		goToView('sendMessageView');		
+		goToView(that.backView());		
 	}	
 	
 }
