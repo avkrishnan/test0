42d3593316d67d3da5cd0ecbee06b9e780007de4
pradeep kumar <pradeep1991singh@gmail.com>
2013-12-26 12:04:06 +0530

    New user sees revised sign-up page [#62958830]

--- Mobile/www/viewModel/SignupStepFirstViewModel.js
+++ Mobile/www/viewModel/SignupStepFirstViewModel.js
@@ -12,37 +12,15 @@ function SignupStepFirstViewModel() {
   this.password = ko.observable();
   this.emailaddress = ko.observable();
 
-  /* Infomation class toggle observable */
-  this.emailhighlight = ko.observable();
-  this.emailunhighlight = ko.observable();
-  this.namehighlight = ko.observable();
-  this.passwordhighlight = ko.observable();
-
-  /* Infomation toggle observable */
-  this.aboutEvernym = ko.observable(true);
-  this.emailInfo = ko.observable(false);
-  this.nameInfo = ko.observable(false);
-  this.passwordInfo = ko.observable(false);
-
   /* Display errors conditionally observable */
   this.errorEmail = ko.observable();
   this.errorAccountName = ko.observable();
   this.errorPassword = ko.observable();
 
-  /* Display error icons observable */
-  this.errorIconEmail = ko.observable();
-  this.errorIconAccountName = ko.observable();
-  this.errorIconPassword = ko.observable();
-
   /* Change error textbox color observable */
   this.emailClass = ko.observable();
   this.accountNameClass = ko.observable();
   this.passwordClass = ko.observable();
-
-  /* Tick icon observable */
-  this.tickIconEmail = ko.observable();
-  this.tickIconAccountName = ko.observable();
-  this.tickIconPassword = ko.observable();
 
 	/* Methods */
   this.applyBindings = function () {
@@ -61,13 +39,7 @@ function SignupStepFirstViewModel() {
     that.errorPassword('');
     that.emailClass('');
     that.accountNameClass('');
-    that.passwordClass('');
-    that.errorIconEmail('');
-    that.errorIconAccountName('');
-    that.errorIconPassword('');
-		that.tickIconEmail('');
-		that.tickIconAccountName('');															
-		that.tickIconPassword('');		
+    that.passwordClass('');	
   };
 
   this.activate = function () {
@@ -80,9 +52,6 @@ function SignupStepFirstViewModel() {
 				that.emailClass('');
 				that.accountNameClass('');
 				that.passwordClass('');
-				that.errorIconEmail('');
-				that.errorIconAccountName('');
-				that.errorIconPassword('');
 			});
 		} 
 		else {
@@ -95,48 +64,6 @@ function SignupStepFirstViewModel() {
 			that.nextViewCommand();
 		}
 	});
-	
-  that.emailinput = function () {
-    that.tickIconEmail('righttick');
-  };
-	
-  that.emailActiveInfo = function () {
-    toggleInfo('nameInfo', 'namehighlight', 'passwordInfo', 'passwordhighlight', 'emailInfo', 'emailhighlight');
-  };
-	
-  that.nameActiveInfo = function () {
-    toggleInfo('emailInfo', 'emailhighlight', 'passwordInfo', 'passwordhighlight', 'nameInfo', 'namehighlight');
-  };
-	
-  that.passwordActiveInfo = function () {
-    toggleInfo('emailInfo', 'emailhighlight', 'nameInfo', 'namehighlight', 'passwordInfo', 'passwordhighlight');
-  };
-
-  /* This function will toggle info on 'i' tap */
-  function toggleInfo(hideElementOne, hideClassOne, hideElementTwo, hideClassTwo, showElement, showClass) {
-    if (that[showElement]() == false) {
-      that.aboutEvernym(false);
-      that[hideElementOne](false);
-      that[hideElementTwo](false);
-      that[showElement](true);
-      that[hideClassOne]('');
-      that[hideClassTwo]('');
-      that[showClass]('enable');
-    } else {
-      that.emailInfo(false);
-      that.nameInfo(false);
-      that.passwordInfo(false);
-      that.emailhighlight('');
-      that.namehighlight('');
-      that.passwordhighlight('');
-    }
-    if (that.emailInfo() == false && that.nameInfo() == false && that.passwordInfo() == false) {
-      that.aboutEvernym(true);
-      that.emailhighlight('');
-      that.namehighlight('');
-      that.passwordhighlight('');
-    }
-  }
 	
 	/* Create Random AccountName Generator */	
   function generateAccount() {
@@ -151,15 +78,12 @@ function SignupStepFirstViewModel() {
     var emailReg = /^[\+_a-zA-Z0-9-]+(\.[\+_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$/;
     if (that.emailaddress() == '' || !emailReg.test(that.emailaddress())) {
       that.emailClass('validationerror');
-      that.errorIconEmail('errorimg');
       that.errorEmail('<span>SORRY:</span> Please enter valid email');
     } else if (that.accountName() == '') {
       that.accountNameClass('validationerror');
-      that.errorIconAccountName('errorimg');
       that.errorAccountName('<span>SORRY:</span> Please enter Evernym name');
     } else if (that.password() == '') {
       that.passwordClass('validationerror');
-      that.errorIconPassword('errorimg');
       that.errorPassword('<span>SORRY:</span> Please enter password');
     } else {
 			$.mobile.showPageLoadingMsg('a', 'Checking Evernym availability');
@@ -170,7 +94,6 @@ function SignupStepFirstViewModel() {
 	function successAvailable(data){
 		if(data){
 			that.accountNameClass('validationerror');
-      that.errorIconAccountName('errorimg');
       that.errorAccountName('<span>SORRY:</span> This Evernym has already been taken');
 		} else {
       $.mobile.showPageLoadingMsg('a', 'Enrolling');
@@ -198,10 +121,7 @@ function SignupStepFirstViewModel() {
     $.mobile.hidePageLoadingMsg();
 		if(details.message == 'communication method already used') {
 			that.emailClass('validationerror');										
-			that.errorIconEmail('errorimg');
 			that.errorEmail('<span>SORRY: </span> ' + details.message);
-			that.tickIconAccountName('righttick');															
-			that.tickIconPassword('righttick');
 		}
   };
 	
