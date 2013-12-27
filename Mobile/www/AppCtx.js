f9803b0cf7e0a82f41bef745248d1c10b517ae73
jlaw <jason@lawcasa.com>
2013-12-27 19:30:40 +0000

    added comments

--- Mobile/www/AppCtx.js
+++ Mobile/www/AppCtx.js
@@ -4,25 +4,28 @@ ENYM.Ctx = function() {
     
   var self = this;
   
+  //store
   self.setItem = function(key, value) {
     //localStorage.setItem(key,value);
     amplify.store(key, value);
     console.log('stored ' + key);
   };
 
+  //clear
   self.removeItem = function(key) {
     //localStorage.removeItem(key);
     amplify.store(key, null);
     console.log('removed ' + key);
   };
 
+  //retrieve
   self.getItem = function(key) {
     //return localStorage.getItem(key);
     console.log('retrieved ' + key);
     var result = amplify.store(key);
     return result == undefined ? null : result;
   };
-
+  
 };
 
 ENYM.ctx = new ENYM.Ctx();
