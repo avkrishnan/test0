df7eb0db252cc1bb2f2330f934e171b9c5525fc3
jlaw <jason@lawcasa.com>
2013-12-27 19:09:10 +0000

    made getItem return null like localStorage does when the value is not found

--- Mobile/www/AppCtx.js
+++ Mobile/www/AppCtx.js
@@ -7,16 +7,20 @@ ENYM.Ctx = function() {
   self.setItem = function(key, value) {
     //localStorage.setItem(key,value);
     amplify.store(key, value);
+    console.log('stored ' + key);
   };
 
   self.removeItem = function(key) {
     //localStorage.removeItem(key);
     amplify.store(key, null);
+    console.log('removed ' + key);
   };
 
   self.getItem = function(key) {
     //return localStorage.getItem(key);
-    return amplify.store(key);
+    console.log('retrieved ' + key);
+    var result = amplify.store(key);
+    return result == undefined ? null : result;
   };
 
 };
