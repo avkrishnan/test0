332127cdbed9c9f857a43d41010d47da72d1ef2d
Devender Singh <devenderdagar@gmail.com>
2013-12-27 17:46:01 +0530

    Code Refactor: VerifyContactView;

--- Mobile/www/viewModel/VerifyContactViewModel.js
+++ Mobile/www/viewModel/VerifyContactViewModel.js
@@ -1,127 +1,93 @@
-﻿/*globals ko*/
-function VerifyContactViewModel() {
-	this.template = "verifyContactView";
-	this.viewid = "V-081";
-	this.viewname = "Verify Contact";
-	this.displayname = "Verify Contact";
-	this.hasfooter = true;
+﻿function VerifyContactViewModel() {
+	var self = this;
+	self.template = "verifyContactView";
+	self.viewid = "V-081";
+	self.viewname = "Verify Contact";
+	self.displayname = "Verify Contact";
+	self.hasfooter = true;
 	
-	this.baseUrl = ko.observable();
-	this.accountName = ko.observable();	
-	this.name = ko.observable();
+  self.inputObs = [ 'baseUrl', 'verificationCommethod', 'verificationCommethodType', 'verificationCommethodID', 'verificationCode', 'navText', 'errorMessage', 'verificationStatus' ];
+  self.defineObservables();		
 	
-	this.verificationCommethod = ko.observable();
-	this.verificationCommethodType = ko.observable();
-	this.verificationCommethodID = ko.observable();
-	this.verificationCode = ko.observable();
-	this.verificationStatus = ko.observable();
+	self.pView = '';
 	
-	this.navText = ko.observable();
-	this.pView = '';
-	this.errorMessage = ko.observable();	
-	
-	var that = this;
-	
-	this.gotoView = function() {
+	self.gotoView = function() {
 		localStorage.removeItem("currentVerificationCommethodID");
 		goToView("addContactView");
-	}
+	};
 	
-	this.verifyRequestCommethod = function() {
-		if(that.verificationCode() == '') {
-			that.errorMessage("<span>ERROR: </span> Please input verification code!");
-		}
-		else if(that.verificationCode().length != 6) {
-			that.errorMessage("<span>ERROR: </span> Verification code should be 6 digits!");
-		}
-		else {
+	self.verifyRequestCommethod = function() {
+		if(self.verificationCode() == '') {
+			self.errorMessage("<span>ERROR: </span> Please input verification code!");
+		} else if(self.verificationCode().length != 6) {
+			self.errorMessage("<span>ERROR: </span> Verification code should be 6 digits!");
+		} else {
 			var verifyCommethodObject = {
-				code : that.verificationCode(),
-				type : that.verificationCommethodType,
-				address : that.verificationCommethod()
+				code : self.verificationCode(),
+				type : self.verificationCommethodType,
+				address : self.verificationCommethod()
 			};
-			that.verifyRequest(verifyCommethodObject);
+			self.verifyRequest(verifyCommethodObject);
 		}
-	}
+	};
 	
-	this.requestVerificationCode = function() {
-		//alert(that.verificationCommethodID());
+	self.requestVerificationCode = function() {
 		var callbacks = {
 			success: function(responseData) {
 				var toastobj = {type: '', text: 'Verification code sent'};
 				showToast(toastobj);
 			},
 			error: function (responseData, status, details) {
-				that.errorMessage("<span>ERROR: </span> " + details.message);
+				self.errorMessage("<span>ERROR: </span> " + details.message);
 			}
 		};		
-		return ES.commethodService.requestVerification(that.verificationCommethodID(), callbacks);
-	}
+		return ES.commethodService.requestVerification(self.verificationCommethodID(), callbacks);
+	};
 	
-	this.inputKeyUp = function(e) {
+	self.inputKeyUp = function(e) {
 		if( e.keyCode != 13 ) {
-			//e.preventDefault();
-			that.errorMessage('');
+			self.errorMessage('');
 		}
 		return true;
-	}	
-  
-	this.applyBindings = function(){
-		$("#" + that.template).on("pagebeforeshow", null, function (e, data) {
-			var currentBaseUrl = localStorage.getItem("baseUrl");
-			var previousView = localStorage.getItem('previousView');
-			console.log("previousView: " + previousView);
-			var vm = ko.dataFor($("#" + previousView).get(0));
-			//alert(JSON.stringify(vm));
-			console.log("previousView Model viewid: " + vm.displayname);
-			that.navText(vm.displayname);
-			that.pView = previousView;
-			that.activate();
-		});
-		$('#verifyContactView input').on("keyup", that.inputKeyUp);
 	};
     
-	this.activate = function() {
-		if(authenticate()) {
-			addExternalMarkup(that.template); // this is for header/overlay message			
-			var _accountName = localStorage.getItem("accountName");
-			var _name = localStorage.getItem("UserFullName");
-			
-			that.accountName(_accountName);	
-			that.name(_name);
-			that.verificationCode('');
-			that.errorMessage('');
-			that.verificationCommethod(localStorage.getItem("currentVerificationCommethod"));
-			that.verificationCommethodID(localStorage.getItem("currentVerificationCommethodID"));
-			that.verificationStatus(localStorage.getItem("verificationStatus"));
-			if (that.verificationStatus() == 'false') {
-				that.verificationStatus(false);
-			}
-			else {
-				that.verificationStatus(true);
-			}
-			if(localStorage.getItem("commethodType") == 'EMAIL') {
-				that.verificationCommethodType('We have sent you a confirmation message. Verify by clicking on the link (or enter the code below).');
-			}
-			else {
-				that.verificationCommethodType('We have sent you a confirmation message. Verify by entering the code below.');
-			}	
-			//alert(localStorage.getItem("currentVerificationCommethod"));
-			//that.getCommethods().then(gotCommethods);
-			
-			$.mobile.showPageLoadingMsg("a", "Loading Settings");
-			return true;
+	self.activate = function() {
+		addExternalMarkup(self.template); // This is for header/overlay message
+		var currentBaseUrl = localStorage.getItem("baseUrl");
+		var previousView = localStorage.getItem('previousView');
+		console.log("previousView: " + previousView);
+		var vm = ko.dataFor($("#" + previousView).get(0));
+		console.log("previousView Model viewid: " + vm.displayname);
+		self.navText(vm.displayname);
+		self.pView = previousView;		
+		$('#verifyContactView input').on("keyup", self.inputKeyUp);
+		self.verificationCode('');
+		self.errorMessage('');
+		self.verificationCommethod(localStorage.getItem("currentVerificationCommethod"));
+		self.verificationCommethodID(localStorage.getItem("currentVerificationCommethodID"));
+		self.verificationStatus(localStorage.getItem("verificationStatus"));
+		if (self.verificationStatus() == 'false') {
+			self.verificationStatus(false);
+		} else {
+			self.verificationStatus(true);
 		}
+		if(localStorage.getItem("commethodType") == 'EMAIL') {
+			self.verificationCommethodType('We have sent you a confirmation message. Verify by clicking on the link (or enter the code below).');
+		} else {
+			self.verificationCommethodType('We have sent you a confirmation message. Verify by entering the code below.');
+		}
+		$.mobile.showPageLoadingMsg("a", "Loading Settings");
+		return true;
 	};
 	
 	$(document).keyup(function(e) {
 		if (e.keyCode == 13 && $.mobile.activePage.attr('id') == 'verifyContactView') {
-			that.errorMessage('');
-			that.verifyRequestCommethod();
+			self.errorMessage('');
+			self.verifyRequestCommethod();
 		}
 	});
 
-	this.verifyRequest = function(verifyCommethodObject) {
+	self.verifyRequest = function(verifyCommethodObject) {
 		var callbacks = {
 			success: function(responseData) {
 				if(localStorage.getItem("commethodType") == 'TEXT') {
@@ -136,11 +102,12 @@ function VerifyContactViewModel() {
 				goToView('addContactView');
 			},
 			error: function (responseData, status, details) {
-				//alert('error');
-				that.errorMessage("<span>ERROR: </span>" + details.message);
+				self.errorMessage("<span>ERROR: </span>" + details.message);
 			}
 		};	
 		return ES.commethodService.verification(verifyCommethodObject.code, callbacks, ES.evernymService.getAccessToken());
 	};
-		
 }
+
+VerifyContactViewModel.prototype = new AppCtx.ViewModel();
+VerifyContactViewModel.prototype.constructor = VerifyContactViewModel;
