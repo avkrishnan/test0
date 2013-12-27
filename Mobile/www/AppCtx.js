1c08c61a25388bafc39de9f3b88d867cd34c9593
jlaw <jason@lawcasa.com>
2013-12-27 18:57:45 +0000

    switched from direct localStorage call to using Amplify.store

--- Mobile/www/AppCtx.js
+++ Mobile/www/AppCtx.js
@@ -5,18 +5,18 @@ ENYM.Ctx = function() {
   var self = this;
   
   self.setItem = function(key, value) {
-    localStorage.setItem(key,value);
-    //amplify.store(key, value);
+    //localStorage.setItem(key,value);
+    amplify.store(key, value);
   };
 
   self.removeItem = function(key) {
-    localStorage.removeItem(key);
-    //amplify.store(key, null);
+    //localStorage.removeItem(key);
+    amplify.store(key, null);
   };
 
   self.getItem = function(key) {
-    return localStorage.getItem(key);
-    //return amplify.store(key);
+    //return localStorage.getItem(key);
+    return amplify.store(key);
   };
 
 };
