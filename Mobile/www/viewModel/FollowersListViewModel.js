c92610a6f91d8bacb996da193afcf8db72c64bea
Devender Singh <devenderdagar@gmail.com>
2013-12-28 12:52:58 +0530

    Code Refactoring: Channels I own, Channel Main View, Followers List View

--- Mobile/www/viewModel/FollowersListViewModel.js
+++ Mobile/www/viewModel/FollowersListViewModel.js
@@ -1,50 +1,36 @@
-﻿/*globals ko*/
-/* To do - Pradeep Kumar */
-function FollowersListViewModel() {	
+﻿function FollowersListViewModel() {
   var that = this;
 	this.template = 'followersListView';
 	this.viewid = 'V-26';
 	this.viewname = 'Followers';
-	this.displayname = 'Followers';	 
-	this.accountName = ko.observable();	
-	
-  /* Followers observable */
-	this.channelId = ko.observable();	
-	this.channelName = ko.observable();			
+	this.displayname = 'Followers';
+
+  self.inputObs = [ 'channelId', 'channelName', 'followerCount'];
+	self.defineObservables();	
   this.followers = ko.observableArray([]);
-	this.followerCount = ko.observable();
-	
-	/* Methods */	
-	this.applyBindings = function() {
-		$('#' + that.template).on('pagebeforeshow', function (e, data) {
-      that.activate();
-    });	
-	};
 	  
 	this.activate = function() {			
-		if(authenticate()) {
-			var channelObject = JSON.parse(ENYM.ctx.getItem('currentChannelData'));				
-			if(!channelObject) {
-				goToView('channelsIOwnView');			
-			} else {				
-				addExternalMarkup(that.template); // this is for header/overlay message	
-				that.followers.removeAll();
-				that.followerCount('0 followers');								
-				that.accountName(ENYM.ctx.getItem('accountName'));		
-				if(ENYM.ctx.getItem('counter') == 1) {
-					ENYM.ctx.setItem('counter', 2);
-				} else if(ENYM.ctx.getItem('counter') == 2){		
-					ENYM.ctx.setItem('counter', 3);
-				}	else {
-					ENYM.ctx.setItem('counter', 1);
-				}																
-				that.channelId(channelObject.channelId);
-				that.channelName(channelObject.channelName);																						
-				$.mobile.showPageLoadingMsg('a', 'Loading Followers');		
-				return ES.channelService.getFollowers(that.channelId(), { success: successfulList, error: errorAPI });
-			}
+		var channelObject = JSON.parse(ENYM.ctx.getItem('currentChannelData'));				
+		if(!channelObject) {
+			goToView('channelsIOwnView');			
+		} else {				
+			addExternalMarkup(that.template); // this is for header/overlay message	
+			that.followers.removeAll();
+			that.followerCount('0 followers');								
+			that.accountName(ENYM.ctx.getItem('accountName'));		
+			if(ENYM.ctx.getItem('counter') == 1) {
+				ENYM.ctx.setItem('counter', 2);
+			} else if(ENYM.ctx.getItem('counter') == 2){		
+				ENYM.ctx.setItem('counter', 3);
+			}	else {
+				ENYM.ctx.setItem('counter', 1);
+			}																
+			that.channelId(channelObject.channelId);
+			that.channelName(channelObject.channelName);																						
+			$.mobile.showPageLoadingMsg('a', 'Loading Followers');		
+			return ES.channelService.getFollowers(that.channelId(), { success: successfulList, error: errorAPI });
 		}
-	}	
+	};	
 	
 	function successfulList(data){
     $.mobile.hidePageLoadingMsg();				
@@ -75,6 +61,5 @@ function FollowersListViewModel() {
 	this.followerDetails = function (data) {
 		ENYM.ctx.setItem('currentfollowerData', JSON.stringify(data));		
 		viewNavigate('Followers', 'followersListView', 'followerDetailsView');
-  };		
-	
+  };
 }
