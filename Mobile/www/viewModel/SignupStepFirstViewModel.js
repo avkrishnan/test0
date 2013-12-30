d04d558efd5eec5b5aed54a276c2de5248151b0e
pradeep1991singh <pradeep1991singh@gmail.com>
2013-12-30 12:08:13 +0530

    help text change on signup view, apply prototype on signup view and placeholder changed on edit name view

--- Mobile/www/viewModel/SignupStepFirstViewModel.js
+++ Mobile/www/viewModel/SignupStepFirstViewModel.js
@@ -1,57 +1,26 @@
-﻿/*globals ko*/
-/* To do - Pradeep Kumar */
-function SignupStepFirstViewModel() {
-  var that = this;
-  this.template = 'signupStepFirstView';
-  this.viewid = 'V-02';
-  this.viewname = 'Register';
-  this.displayname = 'Register';
+﻿function SignupStepFirstViewModel() {
+  var self = this;
+	self.requiresAuth = false;
+  self.template = 'signupStepFirstView';
+  self.viewid = 'V-02';
+  self.viewname = 'Register';
+  self.displayname = 'Register';
 	
-  /* Signup observable */	
-  this.accountName = ko.observable();
-  this.password = ko.observable();
-  this.emailaddress = ko.observable();
-
-  /* Display errors conditionally observable */
-  this.errorEmail = ko.observable();
-  this.errorAccountName = ko.observable();
-  this.errorPassword = ko.observable();
-
-  /* Change error textbox color observable */
-  this.emailClass = ko.observable();
-  this.accountNameClass = ko.observable();
-  this.passwordClass = ko.observable();
-
-	/* Methods */
-  this.applyBindings = function () {
-    $('#' + this.template).on('pagebeforeshow', function (e, data) {
-      that.clearForm();			
-      that.activate();			
-    });
-  };
+  self.inputObs = [ 'evernym', 'password', 'emailaddress'];
+  self.errorObs = [ 'errorEmail', 'errorAccountName', 'errorPassword', 'emailClass', 'accountNameClass', 'passwordClass']
 	
-	this.clearForm = function () {
-    that.accountName('');
-    that.password('');
-    that.emailaddress('');
-    that.errorEmail('');
-    that.errorAccountName('');
-    that.errorPassword('');
-    that.emailClass('');
-    that.accountNameClass('');
-    that.passwordClass('');	
-  };
+	self.defineObservables();	
 
-  this.activate = function () {
+  self.activate = function () {
 		var token = ES.evernymService.getAccessToken();
 		if(token == '' || token == null) {			
 			$('input').keyup(function () {
-				that.errorEmail('');
-				that.errorAccountName('');
-				that.errorPassword('');
-				that.emailClass('');
-				that.accountNameClass('');
-				that.passwordClass('');
+				self.errorEmail('');
+				self.errorAccountName('');
+				self.errorPassword('');
+				self.emailClass('');
+				self.accountNameClass('');
+				self.passwordClass('');
 			});
 		} 
 		else {
@@ -61,49 +30,46 @@ function SignupStepFirstViewModel() {
 	
 	$(document).keyup(function (e) {
 		if (e.keyCode == 13 && $.mobile.activePage.attr('id') == 'signupStepFirstView') {
-			that.nextViewCommand();
+			self.nextViewCommand();
 		}
 	});
 	
 	/* Create Random AccountName Generator */	
   function generateAccount() {
     return {
-      emailaddress: that.emailaddress(),
-      accountname: that.accountName(),		
-      password: that.password()	
+      emailaddress: self.emailaddress(),
+      accountname: self.evernym(),		
+      password: self.password()	
     };
   };	
 	
-  this.nextViewCommand = function () {
+  self.nextViewCommand = function () {
     var emailReg = /^[\+_a-zA-Z0-9-]+(\.[\+_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-zA-Z]{2,4})$/;
-    if (that.emailaddress() == '' || !emailReg.test(that.emailaddress())) {
-      that.emailClass('validationerror');
-      that.errorEmail('<span>SORRY:</span> Please enter valid email');
-    } else if (that.accountName() == '') {
-      that.accountNameClass('validationerror');
-      that.errorAccountName('<span>SORRY:</span> Please enter Evernym name');
-    } else if (that.accountName().length < 5) {
-      that.accountNameClass('validationerror');
-      that.errorAccountName('<span>SORRY:</span> Evernym min. 5 and max. 25 characters');
-		} else if (that.accountName().length > 25) {
-      that.accountNameClass('validationerror');
-      that.errorAccountName('<span>SORRY:</span> Evernym min. 5 and max. 25 characters');			
-    } else if (that.password().length < 8) {
-      that.passwordClass('validationerror');
-      that.errorPassword('<span>SORRY:</span> Password of min. 8 characters');
-		} else if (that.password() == '') {
-      that.passwordClass('validationerror');
-      that.errorPassword('<span>SORRY:</span> Please enter password');
+    if (self.emailaddress() == '' || !emailReg.test(self.emailaddress())) {
+      self.emailClass('validationerror');
+      self.errorEmail('<span>SORRY:</span> Please enter valid email');
+    } else if (self.evernym() == '') {
+      self.accountNameClass('validationerror');
+      self.errorAccountName('<span>SORRY:</span> Please enter Evernym name');
+    } else if (self.evernym().length < 5 || self.evernym().length > 25) {
+      self.accountNameClass('validationerror');
+      self.errorAccountName('<span>SORRY:</span> Evernym min. 5 and max. 25 characters');		
+    } else if (self.password().length < 8) {
+      self.passwordClass('validationerror');
+      self.errorPassword('<span>SORRY:</span> Password of min. 8 characters');
+		} else if (self.password() == '') {
+      self.passwordClass('validationerror');
+      self.errorPassword('<span>SORRY:</span> Please enter password');
     } else {
 			$.mobile.showPageLoadingMsg('a', 'Checking Evernym availability');
-			return ES.loginService.checkName(that.accountName(), { success: successAvailable, error: errorAPI });
+			return ES.loginService.checkName(self.evernym(), { success: successAvailable, error: errorAPI });
     }
   };
 	
 	function successAvailable(data){
 		if(data){
-			that.accountNameClass('validationerror');
-      that.errorAccountName('<span>SORRY:</span> This Evernym has already been taken');
+			self.accountNameClass('validationerror');
+      self.errorAccountName('<span>SORRY:</span> This Evernym has already been taken');
 		} else {
       $.mobile.showPageLoadingMsg('a', 'Enrolling');
       var callbacks = {
@@ -117,32 +83,32 @@ function SignupStepFirstViewModel() {
 	
 	function errorAPI(data, status, details){
 		$.mobile.hidePageLoadingMsg();	
-		that.errorPassword('<span>SORRY:</span> Please enter password');
+		self.errorPassword('<span>SORRY:</span> Please enter password');
 	};	
 	
 
   function signUpSuccess(args) {
     $.mobile.hidePageLoadingMsg();
-		that.loginCommand();
+		self.loginCommand();
   };
 
   function signUpError(data, status, details) {
     $.mobile.hidePageLoadingMsg();
 		if(details.message == 'communication method already used') {
-			that.emailClass('validationerror');										
-			that.errorEmail('<span>SORRY: </span> ' + details.message);
+			self.emailClass('validationerror');										
+			self.errorEmail('<span>SORRY: </span> ' + details.message);
 		}
   };
 	
-	this.loginCommand = function() {
+	self.loginCommand = function() {
     $.mobile.showPageLoadingMsg('a', 'Logging In With New Credentials');
     var callbacks = {
       success : loginSuccess,
       error : loginError
     };
     var loginModel = {};
-    loginModel.accountname = that.accountName();
-    loginModel.password = that.password();
+    loginModel.accountname = self.evernym();
+    loginModel.password = self.password();
     loginModel.appToken = 'sNQO8tXmVkfQpyd3WoNA6_3y2Og=';
     ES.loginService.accountLogin(loginModel, callbacks);
 	}
@@ -152,15 +118,18 @@ function SignupStepFirstViewModel() {
     ES.evernymService.clearAccessToken();
 		ES.evernymService.setAccessToken(args.accessToken);
 		ENYM.ctx.setItem('accountName', args.account.accountname);
-		ENYM.ctx.setItem('newusername', that.accountName());
-		ENYM.ctx.setItem('newuseremail', that.emailaddress());					
+		ENYM.ctx.setItem('newusername', self.evernym());
+		ENYM.ctx.setItem('newuseremail', self.emailaddress());					
 		goToView('registrationVerifyView');			
   }
 
   function loginError(data, status, details) {
     $.mobile.hidePageLoadingMsg();		
-		that.errorFirstLastName('<span>SORRY:</span> '+details.message);
+		self.errorFirstLastName('<span>SORRY:</span> '+details.message);
     ES.evernymService.clearAccessToken();
   }	    	
 				
 }
+
+SignupStepFirstViewModel.prototype = new ENYM.ViewModel();
+SignupStepFirstViewModel.prototype.constructor = SignupStepFirstViewModel;
