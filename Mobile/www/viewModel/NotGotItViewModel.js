3fc46a7633f76f6504eddf17186ec5cbb98131c1
pradeep1991singh <pradeep1991singh@gmail.com>
2014-01-06 12:28:25 +0530

    User sees iGi stats on top of v-56, v-57, and v-34 [#62106466]

--- Mobile/www/viewModel/NotGotItViewModel.js
+++ Mobile/www/viewModel/NotGotItViewModel.js
@@ -7,8 +7,26 @@
 
 	self.recipients = ko.observableArray([]);
 	
-  self.inputObs = [ 'channelId', 'channelName', 'messageId', 'noacks' ];
-  self.defineObservables();	
+  self.inputObs = [
+    'channelName',
+		'channelId',
+		'messageId',
+		'time',
+    'singleMessage', 
+    'broadcastType', 
+    'iGi', 
+    'percentageText',
+    'percentageClass',
+		'percentage',
+		'noiGi',
+		'noacks',
+		'acks',
+		'escalateUntil',
+		'replies'];		
+	self.defineObservables();		
+	self.noacksVisibility = ko.observable(false);
+	self.acksVisibility = ko.observable(false);			
+	self.escalateTime = ko.observable(false);		
 	
 	self.activate = function() {
 		var channelObject = JSON.parse(ENYM.ctx.getItem('currentChannelData'));	
@@ -17,7 +35,36 @@
 			goToView('channelsIOwnView');			
 		} else {
 			addExternalMarkup(self.template); // this is for header/overlay message
-			self.recipients.removeAll();						
+			self.recipients.removeAll();
+			self.time('Sent - '+ formatDate(messageObject.created, 'long'));			
+			self.singleMessage('<span>'+messageObject.broadcastFull+'</span><em></em>');
+			self.broadcastType(messageObject.type);
+			self.iGi(messageObject.iGi);
+			self.percentageText(messageObject.percentageText);
+			self.percentageClass(messageObject.percentageClass);
+			self.percentage(messageObject.percentage);			
+			self.noiGi(messageObject.noiGi);
+			self.noacksVisibility(false);
+			self.acksVisibility(false);			
+			self.escalateTime(false);			
+			if(messageObject.escUntil != '' &&  typeof messageObject.escUntil != 'undefined') {
+				self.escalateTime(true);								
+				self.escalateUntil('<span class="singlemsgicon '+messageObject.sensitivity+'"></span>"'+messageObject.sensitivityText+'" until '+formatDate(messageObject.escUntil, 'short', 'main'));			
+			}
+			self.replies(messageObject.replies);						
+			if(messageObject.type == 'REQUEST_ACKNOWLEDGEMENT' && typeof messageObject.noacks != 'undefined') {
+				if(messageObject.acks+messageObject.noacks == messageObject.acks){
+					self.noacksVisibility(false);
+					self.acksVisibility(true);					
+					self.acks(messageObject.acks+' Got It');										
+				}
+				else {
+					self.noacksVisibility(true);
+					self.acksVisibility(true);	
+					self.noacks(messageObject.noacks+" Haven't Got It Yet");
+					self.acks(messageObject.acks+' Got It');					
+				}
+			}									
 			ENYM.ctx.removeItem('currentRecipientData');									
 			self.channelId(channelObject.channelId);			
 			self.channelName(channelObject.channelName);
@@ -50,6 +97,20 @@
 		var toastobj = {type: 'toast-error', text: details.message};
 		showToast(toastobj);
   };
+	
+	self.showWhoGotIt = function(){
+		if(self.broadcastType() != 'REQUEST_ACKNOWLEDGEMENT') {
+			var toastobj = {type: 'toast-info', text: 'No iGi requested'};
+			showToast(toastobj);						
+		}
+		else if(self.acks() == '0 Got It') {
+			var toastobj = {type: 'toast-info', text: "No iGi's received yet"};
+			showToast(toastobj);			
+		}
+		else {					
+			viewNavigate('Not Got it', 'notGotItView', 'whoGotItView');
+		}
+	};	
 	
 	self.recipientDetails = function(data){
 		ENYM.ctx.setItem('currentRecipientData', JSON.stringify(data));							
--- Mobile/www/viewModel/SingleMessageRepliesViewModel.js
+++ Mobile/www/viewModel/SingleMessageRepliesViewModel.js
@@ -1,14 +1,32 @@
-﻿/* To do - Pradeep Kumar */
-function SingleMessageRepliesViewModel() {
+﻿function SingleMessageRepliesViewModel() {
   var self = this;
 	self.template = 'singleMessageRepliesView';
 	self.viewid = 'V-56';
 	self.viewname = 'Replies';
 	self.displayname = 'Broadcast Replies';	
+		
+	self.msgReplies = ko.observableArray([]);
 	
-	self.inputObs = [ 'channelId', 'channelName', 'messageId'];
+  self.inputObs = [
+    'channelName',
+		'channelId',
+		'messageId',
+		'time',
+    'singleMessage', 
+    'broadcastType', 
+    'iGi', 
+    'percentageText',
+    'percentageClass',
+		'percentage',
+		'noiGi',
+		'noacks',
+		'acks',
+		'escalateUntil',
+		'replies'];		
 	self.defineObservables();		
-	self.replies = ko.observableArray([]);  
+	self.noacksVisibility = ko.observable(false);
+	self.acksVisibility = ko.observable(false);			
+	self.escalateTime = ko.observable(false);	  
 	
 	self.activate = function() {
 		var channelObject = JSON.parse(ENYM.ctx.getItem('currentChannelData'));		
@@ -17,9 +35,36 @@ function SingleMessageRepliesViewModel() {
 			goToView('channelsIOwnView');			
 		} else {
 			addExternalMarkup(self.template); // this is for header/overlay message
-			self.replies.removeAll();										
-			var channelObject = JSON.parse(ENYM.ctx.getItem('currentChannelData'));			
-			var messageObject = JSON.parse(ENYM.ctx.getItem('currentMessageData'));
+			self.msgReplies.removeAll();
+			self.time('Sent - '+ formatDate(messageObject.created, 'long'));			
+			self.singleMessage('<span>'+messageObject.broadcastFull+'</span><em></em>');
+			self.broadcastType(messageObject.type);
+			self.iGi(messageObject.iGi);
+			self.percentageText(messageObject.percentageText);
+			self.percentageClass(messageObject.percentageClass);
+			self.percentage(messageObject.percentage);			
+			self.noiGi(messageObject.noiGi);
+			self.noacksVisibility(false);
+			self.acksVisibility(false);			
+			self.escalateTime(false);			
+			if(messageObject.escUntil != '' &&  typeof messageObject.escUntil != 'undefined') {
+				self.escalateTime(true);								
+				self.escalateUntil('<span class="singlemsgicon '+messageObject.sensitivity+'"></span>"'+messageObject.sensitivityText+'" until '+formatDate(messageObject.escUntil, 'short', 'main'));			
+			}
+			self.replies(messageObject.replies);						
+			if(messageObject.type == 'REQUEST_ACKNOWLEDGEMENT' && typeof messageObject.noacks != 'undefined') {
+				if(messageObject.acks+messageObject.noacks == messageObject.acks){
+					self.noacksVisibility(false);
+					self.acksVisibility(true);					
+					self.acks(messageObject.acks+' Got It');										
+				}
+				else {
+					self.noacksVisibility(true);
+					self.acksVisibility(true);	
+					self.noacks(messageObject.noacks+" Haven't Got It Yet");
+					self.acks(messageObject.acks+' Got It');					
+				}
+			}													
 			ENYM.ctx.removeItem('currentReplyData');													
 			self.channelId(channelObject.channelId);
 			self.channelName(channelObject.channelName);												
@@ -31,7 +76,7 @@ function SingleMessageRepliesViewModel() {
 	
 	function successfulReliesGET(data){
     $.mobile.hidePageLoadingMsg();			
-		for(len = 0; len<data.message.length; len++) {
+		for(var len = 0; len<data.message.length; len++) {
 			//if(data.message[len].replies < 1) {
 				var reply = '<em>'+data.message[len].senderFirstname+' '+data.message[len].senderLastname+': </em>'+data.message[len].text;
 				if(data.message[len].text.length > truncatedTextScreen()) {
@@ -40,7 +85,7 @@ function SingleMessageRepliesViewModel() {
 				else {
 					var replyLess = data.message[len].text;					
 				}				
-				self.replies.push({
+				self.msgReplies.push({
 					replyId: data.message[len].id,
 					senderSubscriberId: data.message[len].senderSubscriberId,
 					responseToMsgId: data.message[len].responseToMsgId,		
@@ -62,6 +107,20 @@ function SingleMessageRepliesViewModel() {
 		var toastobj = {type: 'toast-error', text: details.message};
 		showToast(toastobj);
   };
+	
+	self.showWhoGotIt = function(){
+		if(self.broadcastType() != 'REQUEST_ACKNOWLEDGEMENT') {
+			var toastobj = {type: 'toast-info', text: 'No iGi requested'};
+			showToast(toastobj);						
+		}
+		else if(self.acks() == '0 Got It') {
+			var toastobj = {type: 'toast-info', text: "No iGi's received yet"};
+			showToast(toastobj);			
+		}
+		else {					
+			viewNavigate('Replies', 'singleMessageRepliesView', 'whoGotItView');
+		}
+	};	
 	
 	self.replyDetail = function(data){	
 		ENYM.ctx.setItem('currentReplyData', JSON.stringify(data));									
